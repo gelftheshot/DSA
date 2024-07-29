0b587db29f9e3e77a1133491c2704d14e81ee0eb
Lihon Gebre <113858444+gelftheshot@users.noreply.github.com>
2024-07-29 12:31:20 +0300

    Time: 657 ms (73.61%), Space: 20.7 MB (64.68%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,7 +7,6 @@ class Solution:
         for i in range(n - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
             current = nums[i]
             l, r = i + 1, n - 1
 
